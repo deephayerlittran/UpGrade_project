17fa72a4c3e8787f43a29169ea1512364297bba8
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-16 19:47:49 -0600

    fixed employee controller

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import * as employeeService from "../services/employeeService";
 
-export const getAllEmployees = async (req: Request, res: Response, next: NextFunction) => {
+export const getAll = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const employees = await employeeService.getAllEmployees();
     res.status(200).json(employees);
@@ -10,11 +10,33 @@ export const getAllEmployees = async (req: Request, res: Response, next: NextFun
   }
 };
 
-export const createEmployee = async (req: Request, res: Response, next: NextFunction) => {
+export const getById = async (req: Request, res: Response) => {
+  res.status(501).json({ message: "Not implemented" });
+};
+
+
+
+export const create = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const newEmployee = await employeeService.createEmployee(req.body);
     res.status(201).json(newEmployee);
   } catch (error) {
     next(error);
   }
+};
+
+export const update = async (req: Request, res: Response) => {
+  res.status(501).json({ message: "Not implemented" });
+};
+
+export const remove = async (req: Request, res: Response) => {
+  res.status(501).json({ message: "Not implemented" });
+};
+
+export const employeeController = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
 };
