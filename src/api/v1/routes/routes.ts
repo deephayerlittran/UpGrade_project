3d7fd143fc7f1d750eec3e24f1190e0fa0a54750
Rajandeep20 <Rajandeeplittran@gmail.com>
2025-11-16 18:40:39 -0600

    feat: register department routes in main router

--- src/api/v1/routes/routes.ts
+++ src/api/v1/routes/routes.ts
@@ -1,3 +1,4 @@
+import departmentRoutes from "./departmentRoutes";
 import express from "express";
 import branchRoutes from "./branchRoutes";
 import employeeRoutes from "./employeeRoutes";
@@ -6,5 +7,6 @@ const router = express.Router();
 
 router.use("/branches", branchRoutes);
 router.use("/employees", employeeRoutes);
+router.use("/departments", departmentRoutes);
 
 export default router;
